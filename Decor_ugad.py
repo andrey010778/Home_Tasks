fa17bdfc51b4978cca39df454f48648276ecb188
Андрей <7kuz@mail.ru>
2024-01-02 07:41:30 +0300

    Декорирование "Угадай слово"

---
+++ Decor_ugad.py
@@ -0,0 +1,34 @@
+from random import randint
+
+n_comp = randint(1, 100)
+print(n_comp)
+print('Компьютер "загадал" число в интервале от 1 до 100. Какое?')
+
+
+def counter(func):
+    def wrapper(*args):
+        wrapper.count += 1
+        return func(*args)
+
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def check_numder(numder, n_comp):
+    if numder > n_comp:
+        return 'Загаданное число меньше'
+    elif otvet < n_comp:
+        return 'Загаданное число больше.'
+    else:
+        return otvet
+
+
+while True:
+    otvet = int(input('Введите Ваше число '))
+    answer = check_numder(otvet, n_comp)
+    print(answer)
+    if otvet == n_comp:
+        break
+
+print(f'Правильно! Число попыток отгадывания:', check_numder.count)
