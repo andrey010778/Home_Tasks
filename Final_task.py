2213e4eae82df25cb9ba148c89638f0031d37fe8
Андрей <7kuz@mail.ru>
2024-01-03 18:53:47 +0300

    Декорирование "Угадай слово"

---
+++ Final_task.py
@@ -0,0 +1,29 @@
+
+def selection():
+    print("1. Add")
+    print("2. Edit")
+    print("3. Exit")
+    print()
+    while True:
+        menuselection = int(input("Enter a point of menu"))
+        if menuselection == 1 or menuselection == 2:
+            break
+        elif menuselection == 3:
+            print("Thank you")
+            break
+        else:
+            print("try again")
+    return selection()
+
+selection()
+
+
+
+
+
+
+
+
+
+
+
